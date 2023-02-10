cd09fdb9e31c182225e53bf9d0136052a26146a0
Brandon Thai <brandon88thai@hotmail.com>
2023-02-09 20:37:10 -0500

    Added tiles to the board

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-const body = document.querySelector('body')
+/* eslint-disable linebreak-style */
+const body = document.querySelector('body');
+const board = document.querySelector('.board');
 
+const tiles = [];
+
+for (let i = 0; i < 9; i++) {
+  const b = document.createElement('button');
+  b.classList.add('tile');
+  b.textContent = i+1;
+  tiles.push(b);
+}
+
+tiles[0].setAttribute('style',
+    'border-bottom: 2px solid black; border-right: 2px solid black');
+
+for (const b of tiles) {
+  board.appendChild(b);
+}
