850f3b1278d8d964809fe5d44ad2787461c50476
Brandon Thai <brandon88thai@hotmail.com>
2023-02-10 10:21:11 -0500

    Update board styling to tic-tac-toe

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* eslint-disable linebreak-style */
-const body = document.querySelector('body');
+// const body = document.querySelector('body');
 const board = document.querySelector('.board');
 
 const tiles = [];
@@ -7,12 +7,38 @@ const tiles = [];
 for (let i = 0; i < 9; i++) {
   const b = document.createElement('button');
   b.classList.add('tile');
-  b.textContent = i+1;
   tiles.push(b);
 }
 
+const boardThickness = '2';
+
 tiles[0].setAttribute('style',
-    'border-bottom: 2px solid black; border-right: 2px solid black');
+    `border-bottom: ${boardThickness}px solid black; 
+    border-right: ${boardThickness}px solid black`);
+
+tiles[1].setAttribute('style',
+    `border-bottom: ${boardThickness}px solid;
+    border-right: ${boardThickness}px solid black`);
+
+tiles[2].setAttribute('style',
+    `border-bottom: ${boardThickness}px solid black`);
+
+tiles[3].setAttribute('style',
+    `border-bottom: ${boardThickness}px solid;
+    border-right: ${boardThickness}px solid black`);
+
+tiles[4].setAttribute('style',
+    `border-bottom: ${boardThickness}px solid;
+    border-right: ${boardThickness}px solid black`);
+
+tiles[5].setAttribute('style',
+    `border-bottom: ${boardThickness}px solid black`);
+
+tiles[6].setAttribute('style',
+    `border-right: ${boardThickness}px solid black`);
+
+tiles[7].setAttribute('style',
+    `border-right: ${boardThickness}px solid black`);
 
 for (const b of tiles) {
   board.appendChild(b);
