6790dc977a7a43e300746bb4b59ae0a3e869aa9c
Brandon Thai <brandon88thai@hotmail.com>
2023-02-13 14:56:10 -0500

    Add game functionality for single round

--- script.js
+++ script.js
@@ -4,6 +4,60 @@ const boardDisplay = document.querySelector('.board');
 const scoreText1 = document.querySelector('.p1ScoreText');
 const scoreText2 = document.querySelector('.p2ScoreText');
 
+let p1Turn = true;
+
+// eslint-disable-next-line require-jsdoc
+function onTileClicked() {
+  if (this.textContent != '') return;
+  if (p1Turn) {
+    this.textContent = p1._type;
+    this.style.color = 'blue';
+  } else {
+    this.textContent = p2._type;
+    this.style.color = 'red';
+  }
+  this.style.borderColor = 'black';
+  checkGameOver();
+  p1Turn = !p1Turn;
+}
+
+// eslint-disable-next-line require-jsdoc
+function toggleButtons() {
+  board.tiles.forEach((b) => b.disabled = !b.disabled);
+}
+
+// eslint-disable-next-line require-jsdoc
+function gameOver() {
+//   board.tiles.forEach((b) => b.textContent = '');
+  toggleButtons();
+  //   toggleBoard();
+  (p1Turn) ? p1.wins() : p2.wins();
+  updateScoreBoard(p1, p2);
+}
+
+// eslint-disable-next-line require-jsdoc
+function checkGameOver() {
+  const tiles = board.convertTilesToArray();
+  // rows
+  const rows =
+    (tiles[0] !== '' && tiles[0] == tiles[1] && tiles[0] == tiles[2]) ||
+    (tiles[3] !== '' && tiles[3] == tiles[4] && tiles[3] == tiles[5]) ||
+    (tiles[6] !== '' && tiles[6] == tiles[7] && tiles[6] == tiles[8]);
+  // columns
+  const cols =
+    (tiles[0] !== '' && tiles[0] == tiles[3] && tiles[0] == tiles[6]) ||
+    (tiles[1] !== '' && tiles[1] == tiles[4] && tiles[1] == tiles[7]) ||
+    (tiles[2] !== '' && tiles[2] == tiles[5] && tiles[2] == tiles[8]);
+  // diagonal
+  const diags =
+    (tiles[0] !== '' && tiles[0] == tiles[4] && tiles[0] == tiles[8]) ||
+    (tiles[2] !== '' && tiles[2] == tiles[4] && tiles[2] == tiles[6]);
+
+  if (rows || cols || diags) {
+    gameOver();
+  }
+}
+
 // eslint-disable-next-line require-jsdoc
 class Board {
   // eslint-disable-next-line require-jsdoc
@@ -14,8 +68,12 @@ class Board {
   }
 
   // eslint-disable-next-line require-jsdoc
-  get board() {
-    return this.tiles;
+  convertTilesToArray() {
+    const arr = [];
+    for (const b of this.tiles) {
+      arr.push(b.textContent);
+    }
+    return arr;
   }
 
   // eslint-disable-next-line require-jsdoc
@@ -33,6 +91,7 @@ class Board {
       for (let i = 0; i < 9; i++) {
         const b = document.createElement('button');
         b.classList.add('tile');
+        b.addEventListener('click', onTileClicked);
         this.tiles.push(b);
       }
       this.tiles[0].setAttribute('style',
@@ -105,10 +164,11 @@ const board = new Board();
 
 const p1 = new Player('X'); // can set X to user input in the future
 const p2 = new Player('O');
-p1.wins();
-updateScoreBoard(p1, p2);
-console.log(p1.score);
 
-for (const b of board.board) {
+// p1.wins();
+// updateScoreBoard(p1, p2);
+// console.log(p1.score);
+
+for (const b of board.tiles) {
   boardDisplay.appendChild(b);
 }
