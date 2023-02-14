2f6ca90553738084dfc4df6dc15c0b958f984da3
Brandon Thai <brandon88thai@hotmail.com>
2023-02-14 12:59:02 -0500

    Added comments

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ const scoreText2 = document.querySelector('.p2ScoreText');
 
 let p1Turn = true;
 
-// eslint-disable-next-line require-jsdoc
+/**
+ * Function that is called when a tile is clicked.
+ */
 function onTileClicked() {
+  // ignore if tile that was clicked already has a value
   if (this.textContent != '') return;
+
   if (p1Turn) {
     this.textContent = p1._type;
     this.style.color = 'blue';
@@ -17,21 +21,25 @@ function onTileClicked() {
     this.style.color = 'red';
   }
   this.style.borderColor = 'black';
+
+  // check if the game ends after the tile value changes
   checkGameOver();
+
+  // switch turns
   p1Turn = !p1Turn;
 }
 
-// eslint-disable-next-line require-jsdoc
+/**
+ * Function to enable/disable the buttons on the board.
+ */
 function toggleButtons() {
   board.tiles.forEach((b) => b.disabled = !b.disabled);
 }
 
-// eslint-disable-next-line require-jsdoc
-function toggleBoard() {
-  boardDisplay.style.visibility = 'hidden';
-}
-
-// eslint-disable-next-line require-jsdoc
+/**
+ * Function to remove the game over display and add
+ * back the board div to restart the game.
+ */
 function switchBoardDisplay() {
   body.removeChild(body.lastChild);
 
@@ -41,10 +49,13 @@ function switchBoardDisplay() {
 
   body.appendChild(newBoard);
 
-  board = new Board();
+  board = new Board(); // reset board
 }
 
-// eslint-disable-next-line require-jsdoc
+/**
+ * Function to remove the board and add the
+ * game over display.
+ */
 function addButtons() {
   const div = document.createElement('div');
   div.classList.add('gameOver');
@@ -53,11 +64,12 @@ function addButtons() {
   const buttons = document.createElement('div');
   buttons.classList.add('buttons');
 
+  // add buttons
   const startOverBtn = document.createElement('button');
   startOverBtn.classList.add('startOver');
   startOverBtn.textContent = 'Start Over';
   startOverBtn.addEventListener('click', () => {
-    p1Turn = true;
+    p1Turn = true; // start over resets to p1 turn
     p1.resetScore();
     p2.resetScore();
     updateScoreBoard(p1, p2);
@@ -68,6 +80,7 @@ function addButtons() {
   const playAgainBtn = document.createElement('button');
   playAgainBtn.classList.add('playAgain');
   playAgainBtn.textContent = 'Play Again';
+  // play again restarts the game with the opposite player going first
   playAgainBtn.addEventListener('click', switchBoardDisplay);
   buttons.appendChild(playAgainBtn);
 
@@ -77,17 +90,25 @@ function addButtons() {
   body.appendChild(div);
 }
 
-// eslint-disable-next-line require-jsdoc
+/**
+ * Function that ends the game and updates displays.
+ */
 function gameOver() {
+  // reset the text of the buttons
   board.tiles.forEach((b) => b.textContent = '');
-  toggleButtons();
-  //   toggleBoard();
-  addButtons();
+  toggleButtons(); // disable the buttons
+  addButtons(); // add game over buttons
+
+  // the player that went last and triggered this function
+  // is the player that won the game
   (p1Turn) ? p1.wins() : p2.wins();
-  updateScoreBoard(p1, p2);
+  updateScoreBoard(p1, p2); // update the scoreboard with the new score
 }
 
-// eslint-disable-next-line require-jsdoc
+/**
+ * Function to check if the game should end and calls the
+ * gameOver() function if it does.
+ */
 function checkGameOver() {
   const tiles = board.convertTilesToArray();
   // rows
@@ -110,9 +131,14 @@ function checkGameOver() {
   }
 }
 
-// eslint-disable-next-line require-jsdoc
+/**
+ * Class that represents the tic-tac-toe board.
+ * Keeps track of the tiles and board thickness.
+ */
 class Board {
-  // eslint-disable-next-line require-jsdoc
+  /**
+   * Initializes an empty board
+   */
   constructor() {
     this.tiles = [];
     this._boardThickness = 8;
@@ -120,7 +146,12 @@ class Board {
     this.updateBoardDisplay();
   }
 
-  // eslint-disable-next-line require-jsdoc
+  /**
+   * Function to convert the tiles from a button array
+   * to a string representation.
+   *
+   * @return {Array} a array of strings representing the board
+   */
   convertTilesToArray() {
     const arr = [];
     for (const b of this.tiles) {
@@ -129,24 +160,39 @@ class Board {
     return arr;
   }
 
-  // eslint-disable-next-line require-jsdoc
+  /**
+   * Function that changes the board thickness.
+   * Defaults to 8px but this allows adjustment to
+   * the board if needed.
+   *
+   * @param {number} val the value to change the board thickness to
+   */
   set boardThickness(val) {
     if (typeof(val) === 'number' && val > 0) {
       this._boardThickness = val;
+      this.tiles = [];
+      this.initBoard();
+      this.updateBoardDisplay();
     } else {
       alert(`Could not set board thickness to ${val}`);
     }
   }
 
-  // eslint-disable-next-line require-jsdoc
+  /**
+   * Function that initializes the board with the given
+   * board thickness.
+   */
   initBoard() {
     if (this.tiles.length === 0) {
+      // add the buttons to tiles
       for (let i = 0; i < 9; i++) {
         const b = document.createElement('button');
         b.classList.add('tile');
         b.addEventListener('click', onTileClicked);
         this.tiles.push(b);
       }
+
+      // Set up the board using border of the buttons
       this.tiles[0].setAttribute('style',
           `border-bottom: ${this._boardThickness}px solid black; 
             border-right: ${this._boardThickness}px solid black`);
@@ -177,7 +223,10 @@ class Board {
     }
   }
 
-  // eslint-disable-next-line require-jsdoc
+  /**
+   * Function to add the tiles to the board div on the
+   * document.
+   */
   updateBoardDisplay() {
     boardDisplay = document.querySelector('.board');
     for (const b of this.tiles) {
@@ -186,43 +235,65 @@ class Board {
   }
 };
 
-// eslint-disable-next-line require-jsdoc
+/**
+ * Function to update the scoreboard display with
+ * the new player score values.
+ *
+ * @param {Player} player1 the first player
+ * @param {Player} player2 the second player
+ */
 function updateScoreBoard(player1, player2) {
   scoreText1.textContent = player1.score;
   scoreText2.textContent = player2.score;
 }
 
-// eslint-disable-next-line require-jsdoc
+/**
+ * Class that represents a player and stores
+ * the player's score and mark type (x or o)
+ */
 class Player {
-  // eslint-disable-next-line require-jsdoc
+  /**
+   * Initializes the player with score of 0
+   * @param {String} type the type of mark
+   */
   constructor(type) {
     this._score = 0;
     this._type = type;
   }
 
-  // eslint-disable-next-line require-jsdoc
+  /**
+   * Getter method for the player's mark type
+   */
   get type() {
     return this._type;
   }
 
-  // eslint-disable-next-line require-jsdoc
+  /**
+   * Getter method for the player's score
+   */
   get score() {
     return this._score;
   }
 
-  // eslint-disable-next-line require-jsdoc
+  /**
+   * Function to reset the player's score to 0
+   */
   resetScore() {
     this._score = 0;
   }
 
-  // eslint-disable-next-line require-jsdoc
+  /**
+   * Function to update the player's score if they won
+   */
   wins() {
     this._score += 1;
   }
 };
 
+// make board for the game
 let board = new Board();
 
+// initialize new players
 const p1 = new Player('X'); // can set X to user input in the future
 const p2 = new Player('O');
 
